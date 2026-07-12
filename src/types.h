af29149ca0518e18b231f77b81cb24c031a377b1
Anshkarma <anshkarma03@gmail.com>
2026-07-12 16:08:39 +0530

    added kalman struct

--- src/types.h
+++ src/types.h
@@ -25,4 +25,23 @@ typedef struct {
     float yaw;
 } control_torque_t;
 
+typedef struct {
+float z;      // Estimated altitude (meters)
+float v;      // Estimated vertical velocity (m/s)
+
+// Error Covariance Matrix (2x2)
+float P[2][2];
+
+// Process Noise Covariance (Tuning Parameters)
+float Q_pos;
+float Q_vel;
+
+// Measurement Noise Covariance
+float R_baro;
+
+
+} kalman_alt_t;
+
+
+
 #endif // TYPES_H
